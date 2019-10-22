b633066db2806b85cd465150957d2c2a0f71f125
yudaiukawa <55661117+yudaiukawa@users.noreply.github.com>
2019-10-22 21:56:28 +0900

    Create noname.js

---
+++ noname.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  $("h1").fadeIn(3000);
+});
+
+$('h1').click(function(){
+  $('#details').fadeIn(1500);
+});
+
+$('#work').click(function(){
+  $('.noname').fadeIn(1500);
+})
+
+$('.noname').click(function(){
+  $('.noname').fadeOut(1500);
+})
+
+$('#about').click(function(){
+  $('.miss').fadeIn(1500);
+});
+
+$('.miss').click(function(){
+  $('.miss').fadeOut(1500);
+});
+
+$('#sns').click(function(){
+  $('.sns').fadeIn(1500);
+});
+
+$('.sns').click(function(){
+  $('.sns').fadeOut(1500);
+});
+
+$('#skill').click(function(){
+  $('.skill').fadeIn(1500);
+});
+
+$('.skill').click(function(){
+  $('.skill').fadeOut(1500);
+});
+
+$('#background').click(function(){
+  $('.background').fadeIn(1500);
+});
+
+$('.background').click(function(){
+  $('.background').fadeOut(1500);
+});
+
+$('#contact').click(function(){
+  $('.contacts').fadeIn(1500);
+});
+
+$('#plane').click(function(){
+  $('.contacts').fadeOut(1500);
+});
